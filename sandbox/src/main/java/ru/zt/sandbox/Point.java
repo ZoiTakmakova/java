fcc5e2af6ae3bbeed781bb2aee5d35c809d1a4a5
ZoiTakmakova <41911306+ZoiTakmakova@users.noreply.github.com>
2018-08-08 23:58:12 +0300

    Добавление Расчета расстояния между двумя точками

---
+++ sandbox/src/main/java/ru/zt/sandbox/Point.java
@@ -0,0 +1,12 @@
+package ru.zt.sandbox;
+
+public class Point {
+public double x; //атрибут класса
+public double y; //атрибут класса
+
+public Point(double x, double y) {
+  this.x = x;
+  this.y = y;
+}
+
+}
