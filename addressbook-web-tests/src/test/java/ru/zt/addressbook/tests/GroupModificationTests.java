47eb8dbf312e3a3b91f5e1389e98d97da35933bf
ZoiTakmakova <41911306+ZoiTakmakova@users.noreply.github.com>
2018-08-30 15:45:19 +0300

    Выбор элемента списка по порядковому номеру

--- addressbook-web-tests/src/test/java/ru/zt/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/zt/addressbook/tests/GroupModificationTests.java
@@ -14,9 +14,9 @@ public void testGroupModification(){
       app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
     }
     app.getNavigationHelper().gotoGroupPage();
-    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().selectGroup(before-1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("test11", "test22", "test33"));
+    app.getGroupHelper().fillGroupForm(new GroupData("test12", "test22", "test33"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
     int after = app.getGroupHelper().getGroupCount(); //количество групп после добавления
