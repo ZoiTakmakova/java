c809eeb15d3dd9f7e9a29332921c1e7df4a93bfa
ZoiTakmakova <41911306+ZoiTakmakova@users.noreply.github.com>
2018-09-14 11:27:30 +0300

    Hamcrest улучшение внешнего вида проверок (удаление контактов)

--- addressbook-web-tests/src/test/java/ru/zt/addressbook/tests/ContactDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/zt/addressbook/tests/ContactDeletionTest.java
@@ -1,13 +1,21 @@
 package ru.zt.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.zt.addressbook.model.ContactData;
+import ru.zt.addressbook.model.Contacts;
 import ru.zt.addressbook.model.GroupData;
+import ru.zt.addressbook.model.Groups;
 
 import java.util.List;
 import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
 
 
 public class ContactDeletionTest extends TestBase {
@@ -28,14 +36,13 @@ public void ensurePrecondition() {
 @Test
 public void ContactDeletionTest() {
   app.goTo().homePage();
-  Set<ContactData> before = app.contact().all();
+ Contacts before = app.contact().all();
   ContactData deletedContact = before.iterator().next();
   app.contact().delete(deletedContact);
   app.goTo().homePage();
-  Set<ContactData> after = app.contact().all();
-  Assert.assertEquals(after.size(), before.size() - 1);//проверка: кол-во контактов после удаления должно быть равно кол-ву контактов до удаления  -1
+  Contacts after = app.contact().all();
+  assertEquals(after.size(), before.size() - 1);//проверка: кол-во контактов после удаления должно быть равно кол-ву контактов до удаления  -1
 
-  before.remove(deletedContact);
-  Assert.assertEquals(before, after);
+  assertThat(after, equalTo(before.without(deletedContact)));
 }
 }
