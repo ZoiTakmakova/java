9c7fa1ec542bdfcfea2a5e36dcaec3573cbc7cd8
ZoiTakmakova <41911306+ZoiTakmakova@users.noreply.github.com>
2018-09-14 11:32:28 +0300

    Реализованы проверки в fluent-стиле (контакты и группы)

--- addressbook-web-tests/src/test/java/ru/zt/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/zt/addressbook/tests/ContactModificationTests.java
@@ -1,15 +1,22 @@
 package ru.zt.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.zt.addressbook.model.ContactData;
+import ru.zt.addressbook.model.Contacts;
 import ru.zt.addressbook.model.GroupData;
 
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
 
 public class ContactModificationTests extends TestBase {
 
@@ -31,20 +38,14 @@ public void ensurePrecondition() {
 public void testContactModification() {
 
   app.goTo().homePage();
-  Set<ContactData> before = app.contact().all();//кол-во контактов до модификации контакта
+  Contacts before = app.contact().all();
   ContactData modifiedContact = before.iterator().next();
   ContactData contact = new ContactData().withId(modifiedContact.getId()).withLastname("Ivanov2").withFirstName("Ivan2").withGroup("test1");
   app.contact().modify(contact);
   app.goTo().homePage();
-  Set<ContactData> after = app.contact().all();
-  Assert.assertEquals(after.size(), before.size());//проверка: кол-во контактов после модификации должно быть равно кол-ву контактов до модификации
+  Contacts after = app.contact().all();
+  assertEquals(after.size(), before.size());
 
-
-  before.remove(modifiedContact);
-  before.add(contact);
-
-  Assert.assertEquals(before, after);
+  assertThat(after, equalTo(before.without(modifiedContact).withAdded(contact)));
 }
-
-
 }
