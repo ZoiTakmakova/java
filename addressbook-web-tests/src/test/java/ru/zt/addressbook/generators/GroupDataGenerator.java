de2fe5ca0d553f7f58f96c3fcc10f497552eed4b
ZoiTakmakova <41911306+ZoiTakmakova@users.noreply.github.com>
2018-09-25 14:26:20 +0300

    Генератор тестовых данных, запись данных в файл(completed)

--- addressbook-web-tests/src/test/java/ru/zt/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/zt/addressbook/generators/GroupDataGenerator.java
@@ -3,11 +3,14 @@ package ru.zt.addressbook.generators;
 import ru.zt.addressbook.model.GroupData;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GroupDataGenerator {
-public static void main(String[] args) {
+public static void main(String[] args) throws IOException {
   int count = Integer.parseInt(args[0]);
   File file = new File(args[1]);
 
@@ -15,8 +18,13 @@ public static void main(String[] args) {
   save(groups, file);
 }
 
-private static void save(List<GroupData> groups, File file) {
-
+private static void save(List<GroupData> groups, File file) throws IOException {
+  System.out.println(new File(".").getAbsolutePath());
+  Writer writer = new FileWriter(file);
+  for (GroupData group : groups){
+    writer.write(String.format("%s;%s;%s\n",group.getName(),group.getHeader(),group.getFooter()));
+  }
+  writer.close();
 }
 
 private static List<GroupData> generateGroups(int count) {
