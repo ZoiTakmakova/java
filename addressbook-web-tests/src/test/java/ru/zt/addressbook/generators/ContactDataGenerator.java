d4ba0a73d26ba36c8ddd01103d0ff8477e679764
ZoiTakmakova <41911306+ZoiTakmakova@users.noreply.github.com>
2018-09-26 20:37:29 +0300

    Анализ опций командой строки при запуске генератора данных(контакты)

--- addressbook-web-tests/src/test/java/ru/zt/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/zt/addressbook/generators/ContactDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.zt.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.zt.addressbook.model.ContactData;
 
 import java.io.File;
@@ -10,19 +13,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ContactDataGenerator {
-  //запускаемый файл с функцией, который в качестве параметров принимает массив строк
+
+@Parameter(names = "-c", description = "Contact count")
+public int count;
+
+@Parameter(names = "-f", description = "Target file")
+public String file;
+
+
+//запускаемый файл с функцией, который в качестве параметров принимает массив строк
 //в качестве параметров передается колличество контактов и путь к файлу
   public static void main(String[] args) throws IOException {
-    //1й параметр
-    int count = Integer.parseInt(args[0]);
-    //2й параметр
-    File file = new File(args[1]);
-    //1 часть: генерация данных
-    List<ContactData> contacts = generateContacts(count);
-    //2 часть: запись данных в файл
-    save(contacts, file);
+    //создаеем обект текущего класса
+    ContactDataGenerator generator = new ContactDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+      generator.run();
+    }catch(ParameterException ex){
+      jCommander.usage();
+      return;
+    }
   }
-private static void save(List<ContactData> contacts, File file) throws IOException {
+
+private void run() throws IOException {
+  //1 часть: генерация данных
+  List<ContactData> contacts = generateContacts(count);
+  //2 часть: запись данных в файл
+  save(contacts, new File(file));
+
+}
+
+private void save(List<ContactData> contacts, File file) throws IOException {
   //открытие файла
   Writer writer = new FileWriter(file);
   for (ContactData contact:contacts){
@@ -32,7 +54,7 @@ private static void save(List<ContactData> contacts, File file) throws IOExcepti
   writer.close();
 }
 
-private static List<ContactData> generateContacts(int count) {
+private List<ContactData> generateContacts(int count) {
     List<ContactData> contacts = new ArrayList<ContactData>();
     for (int i=0;i<count;i++){
       contacts.add(new ContactData().withLastname(String.format("Lastname %s", i)).withFirstname(String.format("Firstname %s", i))
