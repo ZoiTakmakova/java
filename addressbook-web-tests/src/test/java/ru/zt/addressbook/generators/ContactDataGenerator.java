6c6c8d58750bbf1185d27192c059a38bc01cafa3
ZoiTakmakova <41911306+ZoiTakmakova@users.noreply.github.com>
2018-09-29 13:37:03 +0300

    Автоматическое закрытие файлов после использования(контакты и группы)

--- addressbook-web-tests/src/test/java/ru/zt/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/zt/addressbook/generators/ContactDataGenerator.java
@@ -29,77 +29,75 @@ public String format;
 
 //запускаемый файл с функцией, который в качестве параметров принимает массив строк
 //в качестве параметров передается колличество контактов и путь к файлу
-  public static void main(String[] args) throws IOException {
-    //создаеем обект текущего класса
-    ContactDataGenerator generator = new ContactDataGenerator();
-    JCommander jCommander = new JCommander(generator);
-    try {
-      jCommander.parse(args);
-      generator.run();
-    }catch(ParameterException ex){
-      jCommander.usage();
-      return;
-    }
+public static void main(String[] args) throws IOException {
+  //создаеем обект текущего класса
+  ContactDataGenerator generator = new ContactDataGenerator();
+  JCommander jCommander = new JCommander(generator);
+  try {
+    jCommander.parse(args);
+    generator.run();
+  } catch (ParameterException ex) {
+    jCommander.usage();
+    return;
   }
+}
 
 private void run() throws IOException {
   //1 часть: генерация данных
   List<ContactData> contacts = generateContacts(count);
   //проверка
-  if (format.equals("csv")){
+  if (format.equals("csv")) {
     //2 часть: запись данных в файл csv
     saveAsCsv(contacts, new File(file));
-  }else if (format.equals("xml")){
+  } else if (format.equals("xml")) {
     //2 часть: запись данных в файл xml
     saveAsXml(contacts, new File(file));
-  }else if (format.equals("json")){
+  } else if (format.equals("json")) {
     //2 часть: запись данных в файл xml
     saveAsJson(contacts, new File(file));
-  }else {
+  } else {
     System.out.println("Unrecognized format" + format);
   }
-  }
+}
 
 private void saveAsJson(List<ContactData> contacts, File file) throws IOException {
   Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
   String json = gson.toJson(contacts);
-  Writer writer = new FileWriter(file);
-  writer.write(json);
-  writer.close();
+  try (Writer writer = new FileWriter(file)) {
+    writer.write(json);
+  }
 }
 
 //1й парамектр: список контактов, который нужно сохранять, 2й параметр файл в который нужно сохранять
 private void saveAsXml(List<ContactData> contacts, File file) throws IOException {
-    //создаем объект типа XStream
+  //создаем объект типа XStream
   XStream xstream = new XStream();
   //подсказка: использовать анатацию класса
-    xstream.processAnnotations(ContactData.class);
+  xstream.processAnnotations(ContactData.class);
   String xml = xstream.toXML(contacts);
-  Writer writer = new FileWriter(file);
-  writer.write(xml);
-  writer.close();
+  try (Writer writer = new FileWriter(file)) {
+    writer.write(xml);
+  }
 }
 
 private void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
   //открытие файла
-  Writer writer = new FileWriter(file);
-  for (ContactData contact:contacts){
-    writer.write(String.format("%s;%s;%s\n",contact.getLastname(),contact.getFirstname(),contact.getGroup()));
+  try (Writer writer = new FileWriter(file)) {
+    for (ContactData contact : contacts) {
+      writer.write(String.format("%s;%s;%s\n", contact.getLastname(), contact.getFirstname(), contact.getGroup()));
+    }
   }
-  //закрытие файла
-  writer.close();
 }
 
 private List<ContactData> generateContacts(int count) {
-    List<ContactData> contacts = new ArrayList<ContactData>();
-    for (int i=0;i<count;i++){
-      contacts.add(new ContactData().withLastname(String.format("Ivanov %s", i)).withFirstname(String.format("Ivan %s", i))
-              .withAddress(String.format("address %s", i)).withEmail_1(String.format("%s@mail.ru", i)).withEmail_2(String.format("%s@mail.ru", i)).withEmail_3(String.format("%s@mail.ru", i)).withHomePhone(String.format("11111%s", i))
-              .withMobilePhone(String.format("22222%s", i)).withWorkPhone(String.format("33333%s", i)).withGroup((String.format("test %s", i))));
-    }
+  List<ContactData> contacts = new ArrayList<ContactData>();
+  for (int i = 0; i < count; i++) {
+    contacts.add(new ContactData().withLastname(String.format("Ivanov %s", i)).withFirstname(String.format("Ivan %s", i))
+            .withAddress(String.format("address %s", i)).withEmail_1(String.format("%s@mail.ru", i)).withEmail_2(String.format("%s@mail.ru", i)).withEmail_3(String.format("%s@mail.ru", i)).withHomePhone(String.format("11111%s", i))
+            .withMobilePhone(String.format("22222%s", i)).withWorkPhone(String.format("33333%s", i)).withGroup((String.format("test %s", i))));
+  }
   return contacts;
 }
-
 
 
 }
