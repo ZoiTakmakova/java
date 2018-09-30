711665f9f1b9055b96c9b2e0ba7cd4edad03e915
ZoiTakmakova <41911306+ZoiTakmakova@users.noreply.github.com>
2018-09-30 20:00:16 +0300

    Ленивая инициализация помощников

--- mantis-tests/src/test/java/ru/zt/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/zt/mantis/appmanager/ApplicationManager.java
@@ -17,9 +17,10 @@ public class ApplicationManager {
 
 
 private final Properties properties;
-WebDriver wd;
+private WebDriver wd;
 
 private String browser;
+private RegistrationHelper registrationHelper;
 
 public ApplicationManager(String browser) {
 
@@ -31,27 +32,41 @@ public ApplicationManager(String browser) {
 public void init() throws IOException {
   String target = System.getProperty("target", "local");
   properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-  if (browser.equals(BrowserType.FIREFOX)) {
-    wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true).setBinary("C://Program Files/Mozilla Firefox/firefox.exe"));
-  } else if (browser.equals(BrowserType.CHROME)) {
-    wd = new ChromeDriver();
-  } else if (browser.equals(BrowserType.IE)) {
-    wd = new InternetExplorerDriver();
-  }
-
-  wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-  wd.get(properties.getProperty("web.baseUrl"));
 }
 
 public void stop(){
-  wd.quit();
+  if (wd!=null){
+    wd.quit();
+  }
 }
 public HttpSession newSession(){
   return new HttpSession(this);
 }
+
 public String getProperty(String key){
   return properties.getProperty(key);
 }
 
+public RegistrationHelper registration() {
+  if(registrationHelper ==null){
+    registrationHelper = new RegistrationHelper(this);
+  }
+  return registrationHelper;
+}
 
+public WebDriver getDriver() {
+  if(wd==null){
+    if (browser.equals(BrowserType.FIREFOX)) {
+      wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true).setBinary("C://Program Files/Mozilla Firefox/firefox.exe"));
+    } else if (browser.equals(BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    } else if (browser.equals(BrowserType.IE)) {
+      wd = new InternetExplorerDriver();
+    }
+
+    wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+    wd.get(properties.getProperty("web.baseUrl"));
+  }
+  return wd;
+}
 }
