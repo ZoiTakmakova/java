b596f603cb6ddd587450d1874f83f757c0ba0486
ZoiTakmakova <41911306+ZoiTakmakova@users.noreply.github.com>
2018-10-04 16:13:18 +0300

    Получение почты на встроенный в тесты почтовый сервер 1

--- mantis-tests/src/test/java/ru/zt/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/zt/mantis/appmanager/RegistrationHelper.java
@@ -1,18 +1,29 @@
 package ru.zt.mantis.appmanager;
 
+import com.sun.javafx.binding.ExpressionHelperBase;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class RegistrationHelper {
-private final ApplicationManager app;
+public class RegistrationHelper extends HelperBase {
+
 private WebDriver wd;
 
 public RegistrationHelper(ApplicationManager app) {
-  this.app = app;
-  wd = app.getDriver();
+  super(app);
 }
 
 public void start(String username, String email)
 {
   wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
+  type(By.name("username"),username);
+  type(By.name("email"),email);
+  click(By.cssSelector("input[value='Signup']"));
+}
+
+public void finish(String cofirmationLink, String password) {
+  wd.get(cofirmationLink);
+  type(By.name("password"),password);
+  type(By.name("password_confirm"),password);
+  click(By.cssSelector("input[value='Update User']"));
 }
 }
